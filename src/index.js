17555626ac22bd33ee675dfd2f0e9bfb9a645a52
natalikoyani <natalikagami@gmail.com>
2023-09-10 16:27:04 +0200

    4

--- src/index.js
+++ src/index.js
@@ -1,30 +1,49 @@
 import axios from "axios";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const BASE_URL = 'https://pixabay.com/api/';
-const API_KEY = '39228988-9f38d0df7f0bcbddd9d36da69';
-
 const searchForm = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
+const loadMoreButton = document.querySelector('.load-more');
+
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '39228988-9f38d0df7f0bcbddd9d36da69';
+let page = 1;
+let per_page = 5;
 
 searchForm.addEventListener('submit', handleSubmit);
+loadMoreButton.addEventListener('click', handleClick);
 
 async function handleSubmit(evt) {
     evt.preventDefault();
-    gallery.innerHTML = '';
     const form = evt.currentTarget;
     const q = form.elements.searchQuery.value.replaceAll(' ', '+');
+    page = 1;
+    gallery.innerHTML = '';
+    loadMoreButton.classList.add('is-hidden');
+    
     try {
       const images = await fetchImages(q);
       if (images.length === 0) {
-          Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+        Notify.failure("Sorry, there are no images matching your search query. Please try again.");
       } else {
-          gallery.innerHTML = createMarkup(images);
+        gallery.innerHTML = createMarkup(images);
+        loadMoreButton.classList.remove('is-hidden');
+        const totalPages = response.data.totalHits / per_page;
+        if(page >= totalPages) {
+          loadMoreButton.classList.add('is-hidden');
+          Notify.info("We're sorry, but you've reached the end of search results.");
+        }
       }
-  } catch(err) {
+    } catch(err) {
       console.error(err);
       Notify.failure("An error occurred while fetching images. Please try again later.");
-  }
+    }
+}
+
+function handleClick() {
+  page +=1
+  fetchImages();
+  gallery.insertAdjacentHTML('beforeend', createMarkup());
 }
 
 async function fetchImages(q) {
@@ -34,8 +53,11 @@ async function fetchImages(q) {
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: true,
+        per_page: per_page,
+        page: page,
     });
     const response = await axios.get(`${BASE_URL}?${searchParams}`);
+    console.log(response);
     return response.data.hits;
 }
 
